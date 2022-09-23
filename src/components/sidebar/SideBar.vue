896f08389f89da45a1c0568bed69160e87f0d62a
chrstntmpg <70249444+mcfltimpug@users.noreply.github.com>
2022-09-23 17:35:40 +0800

    initial side nav with ui
    cant retrieve sidebarlink.vue icons + collapse nav

--- src/components/sidebar/SideBar.vue
+++ src/components/sidebar/SideBar.vue
@@ -1,48 +1,73 @@
 <script>
     import SidebarLink from './SidebarLink.vue'
-    import { collapsed, toggleSidebar, sidebarWidth } from './state'
+    import {
+        collapsed,
+        toggleSidebar,
+        sidebarWidth
+    } from './state'
 
-    export default{
+    export default {
         props: {},
-        components: { SidebarLink},
+        components: {
+            SidebarLink
+        },
         setup() {
-            return { collapsed, toggleSidebar, sidebarWidth }
+            return {
+                collapsed,
+                toggleSidebar,
+                sidebarWidth
+            }
         }
     }
 </script>
 
 <template>
-    
-    <div class="sidebar"
-    :style="{ width: sidebarWidth }">
-        <h1>
-            <span v-if="collapsed">
-                <div>V</div>
-                <div>S</div>
-            </span>
-            <span v-else>Vue Sidebar</span>
-        </h1>
 
-        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
-        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
-        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
-        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
-        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
-        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
-        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
-        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
-        <span
-        class="collapse-icon"
-        :class="{ 'rotate-180' : collapsed }"
-        @click="toggleSidebar"><img src="../../assets/gmail_icon.svg" alt=""></span>
-       
+    <div class="sidebar app shadow-lg flex flex-col" :style="{ width: sidebarWidth } ">
+        <div class="nav-toggle w-full">
+            <span class="collapse-icon cursor-pointer" :class="{ 'rotate-180' : collapsed }" @click="toggleSidebar">
+                <font-awesome-icon icon="fa-solid fa-angles-left " /></span>
+        </div>
+        <div class="nav-logo flex justify-center my-5">
+            <h1>
+                <span v-if="collapsed">
+                    <!-- <div>V</div>
+                    <div>S</div> -->
+                </span>
+                <span v-else><img src="../../assets/assumpta-logo.png" alt="" class="w-24"></span>
+            </h1>
+        </div>
+
+        <div class="nav-user  rounded-r-md assumpta-gradient flex flex-row gap-2 mb-5" >
+            <div class="user-pic">
+                <font-awesome-icon icon="fa-solid fa-user" class="text-2xl" />
+            </div>
+            <div class="user-info text-left">
+                <p class ="text-xs text-neutrals-1 "> Juan Dela Cruz </p>
+                <p class ="text-xs text-neutrals-2"> Cainta</p>
+            </div>
+        </div>
+        <div class="nav-links flex flex-col justify-between h-full">
+            <div class="nav-top-links">
+                <SidebarLink to="/dashboard" icon="fa-solid fa-angles-left">Dashboard</SidebarLink>
+                <SidebarLink to="/" icon="fa-solid fa-angles-left">Inventories</SidebarLink>
+                <SidebarLink to="/" icon="fa-solid fa-angles-left">Barcodes</SidebarLink>
+                <SidebarLink to="/" icon="fa-solid fa-angles-left">Suppliers</SidebarLink>
+                <SidebarLink to="/" icon="fa-solid fa-angles-left">User Roles</SidebarLink>
+            </div>
+
+            <div class="bottom-link">
+                <SidebarLink to="/" icon="fas fa-home">Account</SidebarLink>
+                <SidebarLink to="/" icon="fas fa-home">Logout</SidebarLink>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <style>
-    .sidebar{
-        color:black;
-        background-color: brown;
+    .sidebar {
+        color: black;
         float: left;
         position: fixed;
         z-index: 1;
@@ -54,15 +79,12 @@
         flex-direction: column;
     }
 
-    .collapse-icon{
-        position: absolute;
-        bottom: 0;
-        padding:0.75em;
-        color: white;
+    .collapse-icon { 
+        padding: 0.75em;
         transition: 0.2s linear;
     }
 
-    .rotate-180{
+    .rotate-180 {
         transform: rotate(180deg);
         transition: 0.2s linear;
     }
--- src/components/sidebar/SidebarLink.vue
+++ src/components/sidebar/SidebarLink.vue
@@ -1,28 +1,45 @@
 <script>
-    import { computed } from 'vue'
-    import { useRoute } from 'vue-router'
-    import { collapsed } from './state'
+    import {
+        computed
+    } from 'vue'
+    import {
+        useRoute
+    } from 'vue-router'
+    import {
+        collapsed
+    } from './state'
 
-    export default{
+    export default {
         props: {
-            to: { type: String, required: true },
-            icon: { type: String, required: true }
+            to: {
+                type: String,
+                required: true
+            },
+            icon: {
+                type: String,
+                required: true
+            }
 
         },
         setup(props) {
             const route = useRoute()
             const isActive = computed(() => route.path === props.to)
-            return { isActive, collapsed }
+            return {
+                isActive,
+                collapsed
+            }
         }
     }
 </script>
 
 <template>
-    <router-link :to="to" class="link" :class="{ active: isActive }">
-       <i class="icon" :class="icon" />
-       <span v-if="!collapsed">
-             <slot />
-       </span>
+    <router-link :to="to" class="link text-secondary-6 py-5 pl-3 hover:text-brand-6 hover:border-l-4 hover:border-brand-6 active:border-l-4 active:border-brand-6" :class="{ active: isActive }">
+        <font-awesome-icon class="icon" :class="icon" />
+        <transition name="fade">
+            <span v-if="!collapsed">
+                <slot />
+            </span>
+        </transition>
     </router-link>
 </template>
 
@@ -38,24 +55,25 @@
     }
 
     .link {
-      display:flex;
-      align-items: center;
-      
-      cursor: pointer;
-      position: relative;
-      font-weight: 400;
-      user-select: none;
+        display: flex;
+        align-items: center;
 
-      margin: 0.1em 0;
-      padding: 0.4 em;
-      border-radius: 0.25em;
-      height:1.5em;
+        cursor: pointer;
+        position: relative;
+        font-weight: 400;
+        user-select: none;
 
-      color: white;
-      text-decoration: none;
+        margin: 0.1em 0;
+        padding: 0.4 em;
+        height: 1.5em;
+        text-decoration: none;
     }
 
-    .link:hover{
-        background-color: blue;
+
+
+    .link .icon {
+        flex-shrink: 0;
+        width: 25px;
+        margin-right: 10px;
     }
 </style>
