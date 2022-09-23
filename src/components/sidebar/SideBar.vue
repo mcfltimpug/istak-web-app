942445e357df4457c5a5867220e73d38d6df5b4a
chrstntmpg <70249444+mcfltimpug@users.noreply.github.com>
2022-09-23 15:37:35 +0800

    initial side-nav
    no design

---
+++ src/components/sidebar/SideBar.vue
@@ -0,0 +1,69 @@
+<script>
+    import SidebarLink from './SidebarLink.vue'
+    import { collapsed, toggleSidebar, sidebarWidth } from './state'
+
+    export default{
+        props: {},
+        components: { SidebarLink},
+        setup() {
+            return { collapsed, toggleSidebar, sidebarWidth }
+        }
+    }
+</script>
+
+<template>
+    
+    <div class="sidebar"
+    :style="{ width: sidebarWidth }">
+        <h1>
+            <span v-if="collapsed">
+                <div>V</div>
+                <div>S</div>
+            </span>
+            <span v-else>Vue Sidebar</span>
+        </h1>
+
+        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
+        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
+        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
+        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
+        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
+        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
+        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
+        <SidebarLink to="/" icon="fas fa-home">Home</SidebarLink>
+        <span
+        class="collapse-icon"
+        :class="{ 'rotate-180' : collapsed }"
+        @click="toggleSidebar"><img src="../../assets/gmail_icon.svg" alt=""></span>
+       
+    </div>
+</template>
+
+<style>
+    .sidebar{
+        color:black;
+        background-color: brown;
+        float: left;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        padding: 0.5em;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .collapse-icon{
+        position: absolute;
+        bottom: 0;
+        padding:0.75em;
+        color: white;
+        transition: 0.2s linear;
+    }
+
+    .rotate-180{
+        transform: rotate(180deg);
+        transition: 0.2s linear;
+    }
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home flex flex-col justify-center items-center pt-24 pb-16 ">
+    <div class="text-white uppercase text-sm font-bold px-4 py-1 rounded-full bg-gradient-to-r from-brand-6 to-secondary-6">(On-going)</div>
+    <HelloWorld msg="ISTAK Development"/>
+    
   </div>
 </template>
+
+<style>
+  .home > * {
+    z-index: 100;
+  }
+  .home {
+    background-image: url("https://assumptaclinic.com/wp-content/uploads/2022/08/bg_left.png");
+    background-repeat: repeat;
+    background-attachment: fixed;
+    background-position: center;
+    z-index: 2;
+  }
+
+  .home::after {
+    content: '';
+    position: fixed;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.207);
+    z-index: 1;
+  }
+</style>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
